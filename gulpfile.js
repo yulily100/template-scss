5fcf61f9fce4c0ea99ca9785931e913a7980fb8c
百合佐織 <yulililily100@yahoo.co.jp>
2015-02-12 22:32:35 +0900

    ブラウザシンク

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,25 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
+var browser = require("browser-sync");
+
+gulp.task("server", function() {
+    browser({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
 
 gulp.task("sass", function() {
     gulp.src("sass/**/*scss")
+        .pipe(frontnote())
         .pipe(sass())
         .pipe(autoprefixer())
-        .pipe(gulp.dest("./css"));
+        .pipe(gulp.dest("./css"))
+        .pipe(browser.reload({stream:true}));
+});
+
+gulp.task("default",['server'], function() {
+    gulp.watch("sass/**/*.scss",["sass"]);
 });
