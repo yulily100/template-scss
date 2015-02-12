aba37afc1dac493b826b6eedd88569090372153c
百合佐織 <yulililily100@yahoo.co.jp>
2015-02-12 22:16:53 +0900

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("sass", function() {
     gulp.src("sass/**/*scss")
         .pipe(sass())
+        .pipe(autoprefixer())
         .pipe(gulp.dest("./css"));
 });
