f628a3485b51cd82a8c4cd8223a823e9d7349cac
yulily100 <yulililily100@yahoo.co.jp>
2016-02-18 22:42:17 +0900

    エラー時に止めない

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
 var browser = require("browser-sync");
+var plumber = require("gulp-plumber");
 
 // 起動するやつ
 gulp.task("server", function() {
@@ -15,6 +16,7 @@ gulp.task("server", function() {
 // sass
 gulp.task("sass", function() {
     gulp.src("sass/**/*scss")
+        .pipe(plumber())
         .pipe(sass())
         .pipe(autoprefixer())
         .pipe(gulp.dest("./css"))
